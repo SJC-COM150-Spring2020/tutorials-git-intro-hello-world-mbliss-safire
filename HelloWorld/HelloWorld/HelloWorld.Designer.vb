0f2ab284ee084200706008c6df1d250f834c3f77
mbliss-safire <mbliss@safiredesign.com>
2020-01-30 20:28:39 -0500

    Print Hello World to Text Box
    Used txtName to show Hello World message

--- HelloWorld/HelloWorld/HelloWorld.Designer.vb
+++ HelloWorld/HelloWorld/HelloWorld.Designer.vb
@@ -23,6 +23,7 @@ Partial Class HelloWorld
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.btnPress = New System.Windows.Forms.Button()
+        Me.txtName = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
         '
         'btnPress
@@ -31,25 +32,38 @@ Partial Class HelloWorld
         Me.btnPress.FlatStyle = System.Windows.Forms.FlatStyle.Flat
         Me.btnPress.Font = New System.Drawing.Font("Arial Rounded MT Bold", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.btnPress.ForeColor = System.Drawing.Color.White
-        Me.btnPress.Location = New System.Drawing.Point(207, 245)
+        Me.btnPress.Location = New System.Drawing.Point(106, 108)
         Me.btnPress.Name = "btnPress"
         Me.btnPress.Size = New System.Drawing.Size(205, 61)
         Me.btnPress.TabIndex = 0
         Me.btnPress.Text = "Press Me!"
         Me.btnPress.UseVisualStyleBackColor = False
         '
+        'txtName
+        '
+        Me.txtName.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtName.Font = New System.Drawing.Font("Segoe Script", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtName.Location = New System.Drawing.Point(106, 190)
+        Me.txtName.Multiline = True
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(205, 36)
+        Me.txtName.TabIndex = 1
+        '
         'HelloWorld
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.AliceBlue
-        Me.ClientSize = New System.Drawing.Size(616, 553)
+        Me.ClientSize = New System.Drawing.Size(412, 434)
+        Me.Controls.Add(Me.txtName)
         Me.Controls.Add(Me.btnPress)
         Me.Name = "HelloWorld"
         Me.Text = "Hello"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents btnPress As Button
+    Friend WithEvents txtName As TextBox
 End Class
